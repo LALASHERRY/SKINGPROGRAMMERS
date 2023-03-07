c49ae6f9ec6eee8ca37e4c9af6f54f6f696faedc
SK-AK47 <104360717+LALASHERRY@users.noreply.github.com>
2023-03-07 14:29:55 +0500

    Update Pro.py

--- Pro.py
+++ Pro.py
@@ -1,1 +1,12 @@
-Import Pro32
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://facebook.com/groups/291183553213655/')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import Pro64
+elif bit == '32bit':
+    import Pro32
